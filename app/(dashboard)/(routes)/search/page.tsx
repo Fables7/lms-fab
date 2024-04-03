c97ddab9256043b5807f891b59c2519e551db7ac
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-04-03 21:48:09 +0100

    Categories Query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,33 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
+import { Categories } from "./_components/Categories";
 
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+const SearchPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        {/* <SearchInput /> */}
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories items={categories} />
+        {/* <CoursesList items={courses} /> */}
+      </div>
+    </>
+  );
 };
 
 export default SearchPage;
